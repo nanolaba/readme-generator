d186fd0c8ca2283d08f6e3104bf43023fbeb7c5d
alexander.andriishin <alexander@andriishin.com>
2024-05-08 06:42:31 +0300

    commons-cli lib added

--- src/main/java/com/nanolaba/readme_generator/ReadmeGenerator.java
+++ src/main/java/com/nanolaba/readme_generator/ReadmeGenerator.java
@@ -1,8 +1,25 @@
 package com.nanolaba.readme_generator;
 
+import org.apache.commons.cli.*;
+
 public class ReadmeGenerator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+
+        Option version = new Option("version", "print the version information and exit");
+        options.addOption(version);
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption(version)) {
+            printVersion();
+        }
+
+    }
+
+    private static void printVersion() {
 
     }
 }
