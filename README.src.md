dcaacd33a9cac2713ea53e8dd5e90d78472b1478
alexander.andriishin <alexander@andriishin.com>
2024-05-21 10:21:24 +0300

    TableOfContentsWidget

--- README.src.md
+++ README.src.md
@@ -1,17 +1,47 @@
 <!--@nrg.languages=en,ru-->
 <!--@nrg.defaultLanguage=en-->
-
-# Nanolaba Readme Generator (NRG)
+<!--@nrg.name=**Nanolaba Readme Generator (NRG)**-->
+<!--@nrg.stableVersion=1.0-->
 
 ${nrg.widget:languages}
 
-**Nanolaba Readme Generator (NRG)** — это программа для автоматической генерации файлов в формате<!--ru-->
+# Nanolaba Readme Generator (NRG)
+
+${nrg.name} — это программа для автоматической генерации файлов в формате<!--ru-->
 [Markdown]( https://en.wikipedia.org/wiki/Markdown) на основе файла-прототипа.<!--ru-->
-**Nanolaba Readme Generator (NRG)** — is a program for automatically<!--en-->
+${nrg.name} — is a program for automatically<!--en-->
 generating [Markdown files]( https://en.wikipedia.org/wiki/Markdown) based on a prototype file.<!--en-->
 
 ## Overview<!--en-->
 
+Using the ${nrg.name} program, you can generate separate files for different languages<!--en-->
+in [Markdown format]( https://en.wikipedia.org/wiki/Markdown) based on a single template file.<!--en-->
+The template allows the use of variables and special components - widgets.<!--en-->
+This document is an example of the result of this program.<!--en-->
+<!--en-->
+The latest stable version of the program is **${nrg.stableVersion}**.<!--en-->
+
 ## Краткое описание<!--ru-->
 
-TODO
+С помощью программы ${nrg.name} можно генерировать отдельные файлы для разных языков в формате <!--ru-->
+[Markdown]( https://en.wikipedia.org/wiki/Markdown) на основе единого файла-шаблона.<!--ru-->
+В шаблоне возможно использование переменных, а также специальных компонентов - виджетов. <!--ru-->
+Данный документ является примером результата работы этой программы.<!--ru-->
+<!--ru-->
+Последняя стабильная версия программы - **${nrg.stableVersion}**.<!--ru-->
+
+${nrg.widget:tableOfContents}
+
+## Usage
+
+### Using the Command Line Interface
+
+### Use as a java-library
+
+## Template syntax
+
+### Multilanguage support
+
+### Properties
+
+### Widgets
---
+++ src/main/java/com/nanolaba/nrg/widgets/TableOfContentsWidget.java
@@ -0,0 +1,152 @@
+package com.nanolaba.nrg.widgets;
+
+import com.nanolaba.nrg.core.GeneratorConfig;
+import com.nanolaba.nrg.core.NRGUtil;
+import com.nanolaba.nrg.core.TemplateLine;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class TableOfContentsWidget implements NRGWidget {
+
+    @Override
+    public String getName() {
+        return "tableOfContents";
+    }
+
+    @Override
+    public String getBody(WidgetTag widgetTag, GeneratorConfig config, String language) {
+
+        TOCConfig tocConfig = getTocConfig(widgetTag.getParameters());
+
+        List<Header> allHeaders = new ArrayList<>();
+
+        return (StringUtils.isNotEmpty(tocConfig.getTitle()) ? "#" + tocConfig.getTitle() + System.lineSeparator() : "") +
+               config.getSourceFileBody().lines()
+                       .filter(this::isHeader)
+                       .filter(s -> new TemplateLine(config, s).isLineVisible(language))
+                       .map(s -> new Header(s, tocConfig, allHeaders))
+                       .filter(h -> h.level > 0)
+                       .map(Object::toString)
+                       .collect(Collectors.joining());
+    }
+
+    private TOCConfig getTocConfig(String parameters) {
+        TOCConfig config = new TOCConfig();
+        Map<String, String> map = NRGUtil.parseParametersLine(parameters);
+
+        if (map.containsKey("title")) {
+            config.setTitle(map.get("title"));
+        }
+        if (map.containsKey("ordered")) {
+            config.setOrdered(Boolean.parseBoolean(map.get("ordered")));
+        }
+
+        return config;
+    }
+
+    private boolean isHeader(String line) {
+        return StringUtils.trimToEmpty(line).startsWith("#");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static class TOCConfig {
+        private String title;
+        private boolean ordered;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public TOCConfig setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public boolean isOrdered() {
+            return ordered;
+        }
+
+        public TOCConfig setOrdered(boolean ordered) {
+            this.ordered = ordered;
+            return this;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static class Header {
+
+        public static final Pattern HEADER_PATTERN = Pattern.compile("(#+)(.*)");
+        private String title;
+        private String anchor;
+        private int level;
+        private int number;
+        private final List<Header> headers;
+        private final TOCConfig config;
+
+        public Header(String line, TOCConfig config, List<Header> headers) {
+            this.headers = headers;
+            this.config = config;
+
+            Matcher m = HEADER_PATTERN.matcher(line);
+
+            if (m.find()) {
+                level = m.group(1).length() - 1;
+                title = StringUtils.trimToEmpty(m.group(2));
+                anchor = title.toLowerCase()
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace("(", "")
+                        .replace(")", "")
+                        .replace("https://", "")
+                        .replace("http://", "")
+                        .replace(".", "")
+                        .replace(" ", "-");
+
+                Header prev = getPreviousHeader(level);
+                if (prev != null) {
+                    number = prev.number + 1;
+                } else {
+                    number = 1;
+                }
+
+                headers.add(this);
+            }
+        }
+
+        private Header getPreviousHeader(int level) {
+            for (int i = headers.size() - 1; i >= 0; i--) {
+                Header header = headers.get(i);
+                if (header.level < level) {
+                    return null;
+                }
+                if (header.level == level) {
+                    return header;
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder res = new StringBuilder();
+            res.append("\t".repeat(Math.max(0, level - 1)));
+            if (config.isOrdered()) {
+                res.append(number).append(".");
+            } else {
+                res.append("-");
+            }
+            res.append(" [").append(title).append("](#").append(anchor).append(")");
+            res.append(System.lineSeparator());
+            return res.toString();
+        }
+    }
+}
