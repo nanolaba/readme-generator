652611481e36c0d99480195b668cb3b007128363
alexander.andriishin <alexander@andriishin.com>
2024-05-28 06:02:05 +0300

    tests

--- src/test/java/com/nanolaba/nrg/core/NRGUtilTest.java
+++ src/test/java/com/nanolaba/nrg/core/NRGUtilTest.java
@@ -4,11 +4,37 @@ import com.nanolaba.nrg.DefaultNRGTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Properties;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NRGUtilTest extends DefaultNRGTest {
+
+    @Test
+    public void testMergeProperties() {
+        Properties properties = new Properties();
+        NRGUtil.mergeProperty("a", "b", properties);
+        assertEquals("b", properties.getProperty("a"));
+        assertFalse(getOutAndClear().endsWith("A duplicate property 'a' declaration was detected" + RN));
+
+        NRGUtil.mergeProperty("a", "c", properties);
+        assertEquals("c", properties.getProperty("a"));
+        assertTrue(getOutAndClear().endsWith("A duplicate property 'a' declaration was detected" + RN));
+    }
+
+    @Test
+    public void testUnwrapParameters() {
+        assertEquals("", NRGUtil.unwrapParameterValue(""));
+        assertEquals("", NRGUtil.unwrapParameterValue("''"));
+        assertEquals("", NRGUtil.unwrapParameterValue("\"\""));
+        assertEquals("1", NRGUtil.unwrapParameterValue("\"1\""));
+        assertEquals("1", NRGUtil.unwrapParameterValue("'1'"));
+        assertEquals("'1'", NRGUtil.unwrapParameterValue("\"'1'\""));
+        assertEquals("'", NRGUtil.unwrapParameterValue("\"'\""));
+        assertEquals("\"", NRGUtil.unwrapParameterValue("\"\"\""));
+        assertEquals("\"\"", NRGUtil.unwrapParameterValue("\"\"\"\""));
+        assertEquals("\"123\"", NRGUtil.unwrapParameterValue("\"\"123\"\""));
+    }
 
     @Test
     public void testParseParametersLine() {
@@ -17,7 +43,6 @@ class NRGUtilTest extends DefaultNRGTest {
 
         map = NRGUtil.parseParametersLine("  ");
         assertTrue(map.isEmpty());
-
 
         map = NRGUtil.parseParametersLine("a=\"b\"");
         assertEquals(1, map.size());
