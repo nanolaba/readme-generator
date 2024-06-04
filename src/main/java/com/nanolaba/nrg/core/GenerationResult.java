bdfca9a3f25a4daa4a3de0f8fba2c27de5a717c4
alexander.andriishin <alexander@andriishin.com>
2024-06-05 01:32:57 +0300

    tests

--- src/main/java/com/nanolaba/nrg/core/GenerationResult.java
+++ src/main/java/com/nanolaba/nrg/core/GenerationResult.java
@@ -2,24 +2,19 @@ package com.nanolaba.nrg.core;
 
 public class GenerationResult {
 
-    private String language;
-    private StringBuilder content = new StringBuilder();
+    private final String language;
+    private final StringBuilder content = new StringBuilder();
+
+    public GenerationResult(String language) {
+        this.language = language;
+    }
 
     public String getLanguage() {
         return language;
-    }
-
-    public GenerationResult setLanguage(String language) {
-        this.language = language;
-        return this;
     }
 
     public StringBuilder getContent() {
         return content;
     }
 
-    public GenerationResult setContent(StringBuilder content) {
-        this.content = content;
-        return this;
-    }
 }
