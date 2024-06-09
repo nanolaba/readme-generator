d03a6cef6f9b759e11a3aab7d4d919670603f7ab
alexander.andriishin <alexander@andriishin.com>
2024-06-09 13:24:45 +0300

    Handling multiple properties and widgets in one line

--- src/main/java/com/nanolaba/nrg/widgets/WidgetTag.java
+++ src/main/java/com/nanolaba/nrg/widgets/WidgetTag.java
@@ -3,14 +3,19 @@ package com.nanolaba.nrg.widgets;
 import com.nanolaba.nrg.core.TemplateLine;
 
 public class WidgetTag {
+
     private final TemplateLine line;
     private final String name;
     private final String parameters;
+    private final int start;
+    private final int end;
 
-    public WidgetTag(TemplateLine line, String name, String parameters) {
+    public WidgetTag(TemplateLine line, String name, String parameters, int start, int end) {
         this.line = line;
         this.name = name;
         this.parameters = parameters;
+        this.start = start;
+        this.end = end;
     }
 
     public TemplateLine getLine() {
@@ -23,5 +28,13 @@ public class WidgetTag {
 
     public String getParameters() {
         return parameters;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
     }
 }
